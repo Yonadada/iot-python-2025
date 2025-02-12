01b20da7e4faeae58ef31e2e7f3cf42bee3c9b23
Yonadada <yknda123@gmail.com>
2025-02-12 15:50:16 +0900

    Update py05_blocks.py

--- day08/py05_blocks.py
+++ day08/py05_blocks.py
@@ -11,6 +11,26 @@ import math
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
+class Block:
+    def __init__(self, col, rect, speed = 0):
+        self.col = col
+        self.rect = rect
+        self.speed = speed
+        self.dir = random.randint(-45, 45) + 270 # 랜덤값 생성 225 ~ 315
+
+    def move(self): # 공 움직임
+        # 볼이 움직이는 x축 값을 계속 계산하려면 x는 dir값을 radians으로 변환 후 코사인으로 처리
+        self.rect.centerx += math.cos(math.radians(self.dir)) * self.speed
+        # 볼이 움직이는 y축 값을 계속 계산하려면 y는 dir값을 radians으로 변환 후 사인으로 처리
+        self.rect.centery -= math.sin(math.radians(self.dir)) * self.speed
+        
+    def draw_E(self): # 공을 circle이 아니라 ellipse로 생성
+        pygame.draw.ellipse(Surface, self.col, self.rect) #rect의 위치에 col 색으로     
+        
+    def draw_R(self):
+        pygame.draw.rect(Surface, self.col, self.rect)   
+        
+        
 pygame.init()
 Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 FPSCLOCK = pygame.time.Clock() 
@@ -23,11 +43,19 @@ def main():
     score = 0
     
     BLOCK =[]
+    BALL = Block((200,200,0), Rect(375,650,20,20),10)  # 공 생성, 공 스피드 10으로 시작
+    PADDLE = Block((200,200,0), Rect(375, 700,100,30)) # 공을 맞출 패들 생성
+    
     # 클래스를 생성
     # 무지개색 정보
     colors = [(255,0,0), (255,150,0),(255,228,0),
             (11,201,4),(0,80, 255),(0,0,147),
             (201,0,167)]
+    
+    # 초기에 생성될 블럭들(무지개색으로 아홉개씩, 54개의 블록을 만든다)
+    for y, color in enumerate(colors, start=0): #y 값은 0 ~6
+        for x in range(0,9):
+            BLOCK.append(Block(color, Rect(x*80 + 150, y*40 + 40, 60, 20)))
     
     bigFont = pygame.font.SysFont('NanumGothic', 80)
     smallFont = pygame.font.SysFont('NanumGothic', 45)
@@ -35,7 +63,9 @@ def main():
     M_GAME_SUBTITLE = smallFont.render('--PRESS SPACE_BAR--', True, 'white')
     M_CLEAR = bigFont.render('CLEAR!!', True, 'yellow')
     M_FAIL = bigFont.render('FAILED', True, 'red')    
+    
     while True:
+        # 스코어, 스피드 
         Surface.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -43,9 +73,17 @@ def main():
                 sys.exit()  
             elif event.type == KEYDOWN:
                 if event.key == K_LEFT:
-                    pass
+                    if PADDLE.rect.centerx < 55:
+                        PADDLE.rect.centerx = 55
+                    else:
+                        PADDLE.rect.centerx -= 10 #패달은 왼쪽,오른쪽으로만 이동
+
                 elif event.key == K_RIGHT:
-                    pass
+                    if PADDLE.rect.centerx > 945:
+                        PADDLE.rect.centerx = 945
+                    else:
+                        PADDLE.rect.centerx += 10
+                    
                 elif event.key == K_SPACE:
                     is_game_start = True # 게임시작
         
@@ -57,9 +95,36 @@ def main():
                                         (SCREEN_HEIGHT / 2) + 50))
         
         else : # 게임 시작 후 블록,볼,바가 움직이도록 처리
-            Surface.blit(M_CLEAR, (80, 280))    
-        
+            # Surface.blit(M_CLEAR, (80, 280))    
+            
+            LenBlock = len(BLOCK) # 54으로 시작하지만 공에 충돌해서 갯수가 계속 줄어듬
+            # Collision Detection
+            BLOCK = [x for x in BLOCK if not x.rect.colliderect(BALL.rect)]
+            if len(BLOCK) != LenBlock: # 공이 블럭에 맞아서 개수가 맞지 않음
+                BALL.dir *= -1 # 공의 방향이 바뀜
                 
+            if BALL.rect.centery < 1000:
+                BALL.move()
+                
+                
+            # 패들과 공이 부딪힘(Collision Detect!)    
+            if PADDLE.rect.colliderect(BALL.rect):
+                BALL.speed += 0.25 
+                BALL.dir = 90 + (PADDLE.rect.centerx - BALL.rect.centerx) / PADDLE.rect.width * 100   
+                
+                
+            if BALL.rect.centerx < 0 or BALL.rect.centerx > 1000: # 게임화면 양쪽 벽 밖으로 못나가게
+                BALL.dir = 180 - BALL.dir # 반사각만큼 방향 전환
+                
+            elif BALL.rect.centery < 0 : ## 게임화면 천장에 부딪히면 반사
+                BALL.dir = -BALL.dir   
+                
+            BALL.draw_E()
+            PADDLE.draw_R()
+            
+            for i in BLOCK: # Block()
+                i.draw_R()    
+                                
         pygame.display.update()  
         FPSCLOCK.tick(30)
         
